3a6de2ffdeaffc510133b9dd800b525c24b18d10
bramowli <149688574+bramowli@users.noreply.github.com>
2024-03-29 14:38:16 +0100

    fertig

--- A01_Poem/script.js
+++ A01_Poem/script.js
@@ -1,16 +1,30 @@
 "use strict";
 var Poem;
 (function (Poem) {
-    let object = ["the knight", "the queen", "a beggar", "a dog", ""];
-    let predicate = ["abuses", "rules over", "manipulates", "annihilates", "feeds"];
-    let subject = ["fire", "a prior", "the shire", "a liar", "the squire"];
+    const subject = ["the knight", "the queen", "a beggar", "a dog", "the magician"];
+    const predicate = ["abuses", "rules over", "manipulates", "annihilates", "feeds"];
+    const object = ["fire", "a prior", "the shire", "a liar", "the squire"];
+    /*
+    console.log(subject);
+    console.log(predicate);
+    console.log(object);
     console.log(getVerse(object, predicate, subject));
+  */
     for (let i = object.length; i >= 1; i--) {
-        getVerse();
+        //console.log(i);
+        let verse = getVerse(subject, predicate, object);
+        console.log(verse);
     }
-    function getVerse(_object, _predicate, _subject) {
-        return "blub";
+    function getVerse(_subject, _predicate, _object) {
+        let verseAgain = "";
+        let random = Math.floor(Math.random() * _subject.length);
+        verseAgain += _subject.splice(random, 1)[0] + " ";
+        random = Math.floor(Math.random() * _predicate.length);
+        verseAgain += _predicate.splice(random, 1)[0] + " ";
+        //console.log(math);
+        random = Math.floor(Math.random() * _object.length);
+        verseAgain += _object.splice(random, 1)[0];
+        return verseAgain;
     }
-    console.log();
 })(Poem || (Poem = {}));
 //# sourceMappingURL=script.js.map
--- A01_Poem/script.ts
+++ A01_Poem/script.ts
@@ -1,17 +1,30 @@
 namespace Poem {
-    let object: string[] = ["the knight", "the queen", "a beggar", "a dog", ""];
-    let predicate: string[] = ["abuses", "rules over", "manipulates", "annihilates", "feeds"];
-    let subject: string[] = ["fire", "a prior", "the shire", "a liar", "the squire"];
+  const subject: string[] = ["the knight", "the queen", "a beggar", "a dog", "the magician"];
+  const predicate: string[] = ["abuses", "rules over", "manipulates", "annihilates", "feeds"];
+  const object: string[] = ["fire", "a prior", "the shire", "a liar", "the squire"];
 
-    console.log(getVerse(object, predicate, subject))
+  /*
+  console.log(subject);
+  console.log(predicate);
+  console.log(object);
+  console.log(getVerse(object, predicate, subject));
+*/
 
-    for(let i = object.length; i>=1; i--) {
-        getVerse()
-    }
+  for (let i = object.length; i >= 1; i--) {
+    //console.log(i);
+    let verse: string = getVerse(subject, predicate, object);
+    console.log(verse);
+  }
 
-    function getVerse(_object: string[], _predicate: string[], _subject: string[]): string{
-        return "blub";
-    }
-
-    console.log()
+  function getVerse(_subject: string[], _predicate: string[], _object: string[]): string {
+    let verseAgain: string = "";
+    let random: number = Math.floor(Math.random() * _subject.length);
+    verseAgain += _subject.splice(random, 1)[0] + " ";
+    random = Math.floor(Math.random() * _predicate.length);
+    verseAgain += _predicate.splice(random, 1)[0] + " ";
+    //console.log(math);
+    random = Math.floor(Math.random() * _object.length);
+    verseAgain += _object.splice(random, 1)[0];
+    return verseAgain;
+  }
 }
