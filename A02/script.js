e07c435b2c311d1d083417dbf1a793d93dc1fdd1
bramowli <149688574+bramowli@users.noreply.github.com>
2024-04-11 12:59:44 +0200

    bisschen ts

--- A02/script.js
+++ A02/script.js
@@ -1,12 +1,24 @@
 "use strict";
-window.addEventListener("load", handleLoad);
-function handleLoad() {
-    document.addEventListener("mousemove", setInfoBox);
-}
-function setInfoBox(_event) {
-    const box = document.getElementsByTagName("span");
-}
-function logInfo(_event) {
-    console.log(_event.type, _event.target, _event.currentTarget, _event);
-}
+var EventInspector;
+(function (EventInspector) {
+    let span = document.getElementsByTagName("span"), innerHTML;
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        document.getElementById("div0")?.addEventListener("click", logInfo);
+        document.getElementById("div1")?.addEventListener("click", logInfo);
+    }
+    function setInfoBox(_event) {
+        let X = _event.clientX;
+        let Y = _event.clientY;
+        span.innerHTML = _event.clientX, _event.clientY, _event.target;
+    }
+    function logInfo(_event) {
+        console.log(_event.type, _event.target, _event.currentTarget, _event);
+    }
+})(EventInspector || (EventInspector = {}));
 //# sourceMappingURL=script.js.map
--- A02/script.ts
+++ A02/script.ts
@@ -1,13 +1,25 @@
+namespace EventInspector {
+let span = document.getElementsByTagName("span")innerHTML
+
 window.addEventListener("load", handleLoad);
 
 function handleLoad(): void {
-    document.addEventListener("mousemove", setInfoBox)
+    document.addEventListener("mousemove", setInfoBox);
+    document.addEventListener("click", logInfo);
+    document.addEventListener("keyup", logInfo);
+    document.body.addEventListener("click", logInfo);
+    document.body.addEventListener("keyup", logInfo);
+    document.getElementById("div0")?.addEventListener("click", logInfo);
+    document.getElementById("div1")?.addEventListener("click", logInfo);
 }
 
 function setInfoBox(_event: MouseEvent) {
-    const box = document.getElementsByTagName("span")
+    let X: number = _event.clientX;
+    let Y: number = _event.clientY;
+    span.innerHTML = _event.clientX, _event.clientY, _event.target
 }
 
 function logInfo(_event: Event) {
-    console.log(_event.type, _event.target, _event.currentTarget, _event)
+    console.log(_event.type, _event.target, _event.currentTarget, _event);
+}
 }
