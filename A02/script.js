2bb310448884b0830d8c87c5b52cd2b46a50c403
bramowli <149688574+bramowli@users.noreply.github.com>
2024-04-12 23:47:50 +0200

    fertig

--- A02/script.js
+++ A02/script.js
@@ -1,7 +1,6 @@
 "use strict";
 var EventInspector;
 (function (EventInspector) {
-    let span = document.getElementsByTagName("span"), innerHTML;
     window.addEventListener("load", handleLoad);
     function handleLoad() {
         document.addEventListener("mousemove", setInfoBox);
@@ -9,13 +8,22 @@ var EventInspector;
         document.addEventListener("keyup", logInfo);
         document.body.addEventListener("click", logInfo);
         document.body.addEventListener("keyup", logInfo);
-        document.getElementById("div0")?.addEventListener("click", logInfo);
+        document.getElementById("div0")?.addEventListener("click", logInfo); // ? is to check if the id element exists, to avoid runtime errors
+        document.getElementById("div0")?.addEventListener("keyup", logInfo);
         document.getElementById("div1")?.addEventListener("click", logInfo);
+        document.getElementById("div1")?.addEventListener("keyup", logInfo);
     }
     function setInfoBox(_event) {
+        let span = document.getElementById("span");
         let X = _event.clientX;
         let Y = _event.clientY;
-        span.innerHTML = _event.clientX, _event.clientY, _event.target;
+        let offsetX = 10;
+        let offsetY = 10;
+        if (span) {
+            span.innerHTML = `${X}, ${Y}, ${_event.target}`; // insert variables in string
+            span.style.left = _event.clientX + offsetX + "px";
+            span.style.top = _event.clientY + offsetY + "px";
+        }
     }
     function logInfo(_event) {
         console.log(_event.type, _event.target, _event.currentTarget, _event);
--- A02/script.ts
+++ A02/script.ts
@@ -1,25 +1,32 @@
 namespace EventInspector {
-let span = document.getElementsByTagName("span")innerHTML
+  window.addEventListener("load", handleLoad);
 
-window.addEventListener("load", handleLoad);
-
-function handleLoad(): void {
+  function handleLoad(): void {
     document.addEventListener("mousemove", setInfoBox);
     document.addEventListener("click", logInfo);
     document.addEventListener("keyup", logInfo);
     document.body.addEventListener("click", logInfo);
     document.body.addEventListener("keyup", logInfo);
-    document.getElementById("div0")?.addEventListener("click", logInfo);
+    document.getElementById("div0")?.addEventListener("click", logInfo); // ? is to check if the id element exists, to avoid runtime errors
+    document.getElementById("div0")?.addEventListener("keyup", logInfo);
     document.getElementById("div1")?.addEventListener("click", logInfo);
-}
+    document.getElementById("div1")?.addEventListener("keyup", logInfo);
+  }
 
-function setInfoBox(_event: MouseEvent) {
+  function setInfoBox(_event: MouseEvent) {
+    let span: HTMLElement | null = document.getElementById("span");
     let X: number = _event.clientX;
     let Y: number = _event.clientY;
-    span.innerHTML = _event.clientX, _event.clientY, _event.target
-}
+    let offsetX: number = 10;
+    let offsetY: number = 10;
+    if (span) {
+      span.innerHTML = `${X}, ${Y}, ${_event.target}`; // insert variables in string
+      span.style.left = _event.clientX + offsetX + "px";
+      span.style.top = _event.clientY + offsetY + "px";
+    }
+  }
 
-function logInfo(_event: Event) {
+  function logInfo(_event: Event) {
     console.log(_event.type, _event.target, _event.currentTarget, _event);
-}
+  }
 }
