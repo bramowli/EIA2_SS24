b6d4a7148af4e9767d2371b08aedbc6fab1e78d7
bramowli <149688574+bramowli@users.noreply.github.com>
2024-04-27 16:02:39 +0200

    Update Data.ts

--- A03_Einkaufsliste/Data.ts
+++ A03_Einkaufsliste/Data.ts
@@ -1,30 +1,37 @@
 namespace Einkaufsliste {
-  export interface liste{
-    datalist: string
-    date: number
-    amount: number
-    comment: string
+  export interface Listeneintrag {
+    datalist: string;
+    date: string;
+    amount: number;
+    comment: string;
   }
 
-  
-  export let items = [
+  export function logItems() {
+    let output = "";
+    for (let i: number = 0; i < items.length; i++) {
+      output += items[i]?.datalist + "; ";
+    }
+    console.log("Data: " + output);
+  }
+
+  export let items: Listeneintrag[] | undefined[] = [
     {
       datalist: "Klopapier",
-      date: "20.04.24",
-      amount: "8",
+      date: "2024-04-20",
+      amount: 8,
       comment: "Blätter, unbenutzt",
     },
     {
       datalist: "Kind",
-      date: "20.04.24",
-      amount: "1",
+      date: "2024-04-20",
+      amount: 1,
       comment: "lebend oder noch frisch",
     },
     {
-        datalist: "Klopapier",
-        date: "09.09.09",
-        amount: "99",
-        comment: "Tropfen",
-      },
+      datalist: "Wasser",
+      date: "2009-09-09",
+      amount: 99,
+      comment: "Tropfen",
+    },
   ];
 }
