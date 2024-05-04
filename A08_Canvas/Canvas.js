27e33531db812721c728c50362d7d5f585c97c99
bramowli <149688574+bramowli@users.noreply.github.com>
2024-05-04 18:41:57 +0200

    ist jetzt Kunst

--- A08_Canvas/Canvas.js
+++ A08_Canvas/Canvas.js
@@ -5,31 +5,45 @@ var Canvas;
     let crc = cvs.getContext("2d");
     crc.fillRect(0, 0, crc.canvas.width, crc.canvas.height);
     crc.strokeStyle = "#FFFFFF";
-    let rad = Math.floor(Math.random() * (50 - 3)) + 3;
-    let x = Math.floor(Math.random() * (50 - 3)) + 3;
-    let y = Math.floor(Math.random());
+    let rndAmount = Math.floor(Math.random() * 37);
     let gradient = crc.createLinearGradient(0, 0, 500, 500);
     gradient.addColorStop(0, "#34a38f");
     gradient.addColorStop(1, "#5f34a3");
-    let radGradient = crc.createRadialGradient(0, 0, 0, 0, 0, x);
+    let radGradient = crc.createRadialGradient(0, 0, 0, 0, 0, 70);
     radGradient.addColorStop(0, "#9c492260");
     radGradient.addColorStop(1, "#9c4922");
     crc.fillStyle = gradient;
     crc.fillRect(0, 0, 500, 500);
-    let circle = new Path2D();
-    //let v: number = Math.floor(Math.random() * 50 + 3);
-    circle.arc(0, 0, rad, 0, 2 * Math.PI);
-    circle.closePath();
-    crc.fillStyle = radGradient;
-    crc.fill(circle);
-    crc.save();
-    // crc.translate(Math.random(), Math.random());
-    let triCount = Math.floor(Math.random() + 10);
+    while (rndAmount >= 0) {
+        let y = Math.floor(Math.random() * cvs.width);
+        let v = Math.floor(Math.random() * cvs.height);
+        crc.save();
+        crc.translate(v, y);
+        crc.fill(getCircle());
+        rndAmount--;
+        crc.restore();
+    }
+    const random = Math.floor(Math.random() * 2);
+    let color;
+    switch (random) {
+        case 0:
+            color = "#1a4f46";
+            break;
+        case 1:
+            color = "#4f1a2e";
+            break;
+    }
+    crc.fillStyle = color;
     for (let i = 0; i < cvs.width; i += Math.random() * 30 + 55) {
         for (let j = 0; j < cvs.height; j += Math.random() * 30 + 55) {
             crc.save();
             crc.translate(i, j);
-            crc.stroke(getTriangle(50));
+            if (Math.random() <= 0.25) {
+                crc.stroke(getTriangle(50));
+            }
+            else if (Math.random() <= 0.25) {
+                crc.fill(getTriangle(100));
+            }
             crc.restore();
         }
     }
@@ -40,6 +54,14 @@ var Canvas;
         triangle.lineTo(Math.random() * _size, Math.random() * _size);
         triangle.closePath();
         return triangle;
+    }
+    function getCircle() {
+        let rad = Math.floor(Math.random() * 50) + 3;
+        let circle = new Path2D();
+        circle.arc(0, 0, rad, 0, 2 * Math.PI);
+        circle.closePath();
+        crc.fillStyle = radGradient;
+        return circle;
     }
 })(Canvas || (Canvas = {}));
 //# sourceMappingURL=Canvas.js.map
--- A08_Canvas/Canvas.ts
+++ A08_Canvas/Canvas.ts
@@ -5,39 +5,53 @@ namespace Canvas {
   crc.fillRect(0, 0, crc.canvas.width, crc.canvas.height);
   crc.strokeStyle = "#FFFFFF";
 
-  let rad: number = Math.floor(Math.random() * (50 - 3)) + 3;
-  let x: number = Math.floor(Math.random() * (50 - 3)) + 3;
-  let y: number = Math.floor(Math.random());
+  let rndAmount: number = Math.floor(Math.random() * 37);
 
   let gradient: CanvasGradient = crc.createLinearGradient(0, 0, 500, 500);
 
   gradient.addColorStop(0, "#34a38f");
   gradient.addColorStop(1, "#5f34a3");
 
-  let radGradient: CanvasGradient = crc.createRadialGradient(0, 0, 0, 0, 0, x);
+  let radGradient: CanvasGradient = crc.createRadialGradient(0, 0, 0, 0, 0, 70);
   radGradient.addColorStop(0, "#9c492260");
   radGradient.addColorStop(1, "#9c4922");
 
   crc.fillStyle = gradient;
   crc.fillRect(0, 0, 500, 500);
 
-  let circle: Path2D = new Path2D();
-  //let v: number = Math.floor(Math.random() * 50 + 3);
-  circle.arc(0, 0, rad, 0, 2 * Math.PI);
-  circle.closePath();
-  crc.fillStyle = radGradient;
-  crc.fill(circle);
+  while (rndAmount >= 0) {
+    let y: number = Math.floor(Math.random() * cvs.width);
+    let v: number = Math.floor(Math.random() * cvs.height);
+    crc.save();
+    crc.translate(v,y);
+    crc.fill(getCircle());
+    rndAmount--;
+    crc.restore();
+  }
 
-  crc.save();
-  // crc.translate(Math.random(), Math.random());
+  const random = Math.floor(Math.random() * 2);
+  let color: string;
 
-  let triCount = Math.floor(Math.random() + 10);
+  switch (random) {
+    case 0:
+      color = "#1a4f46";
+      break;
+    case 1:
+      color = "#4f1a2e";
+      break;
+  }
+
+  crc.fillStyle = color;
 
   for (let i = 0; i < cvs.width; i += Math.random() * 30 + 55) {
     for (let j = 0; j < cvs.height; j += Math.random() * 30 + 55) {
       crc.save();
       crc.translate(i, j);
-      crc.stroke(getTriangle(50));
+      if (Math.random() <= 0.25) {
+        crc.stroke(getTriangle(50));
+      } else if (Math.random() <= 0.25) {
+        crc.fill(getTriangle(100));
+      }
       crc.restore();
     }
   }
@@ -49,5 +63,14 @@ namespace Canvas {
     triangle.lineTo(Math.random() * _size, Math.random() * _size);
     triangle.closePath();
     return triangle;
+  }
+
+  function getCircle(): Path2D {
+    let rad: number = Math.floor(Math.random() * 50) + 3;
+    let circle: Path2D = new Path2D();
+    circle.arc(0, 0, rad, 0, 2 * Math.PI);
+    circle.closePath();
+    crc.fillStyle = radGradient;
+    return circle;
   }
 }
